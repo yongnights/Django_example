b04bd0305a2f7777242cdb7050efce21bc2864e9
Xxx <xxx@xxx.com>
2020-04-29 16:42:01 +0800

    第一个Django应用

---
+++ polls/urls.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" 
+                                                    __----~~~~~~~~~~~------___
+                                   .  .   ~~//====......          __--~ ~~
+                   -.            \_|//     |||\\  ~~~~~~::::... /~
+               ___-==_       _-~o~  \/    |||  \\            _/~~-
+        __---~~~.==~||\=_    -_--~/_-~|-   |\\   \\        _/~
+    _-~~     .=~    |  \\-_    '-~7  /-   /  ||    \      /
+  .~       .~       |   \\ -_    /  /-   /   ||      \   /
+ /  ____  /         |     \\ ~-_/  /|- _/   .||       \ /
+ |~~    ~~|--~~~~--_ \     ~==-/   | \~--===~~        .\
+          '         ~-|      /|    |-~\~~       __--~~
+                      |-~~-_/ |    |   ~\_   _-~            /\
+                           /  \     \__   \/~                \__
+                       _--~ _/ | .-~~____--~-/                  ~~==.
+                      ((->/~   '.|||' -_|    ~~-/ ,              . _||
+                                 -_     ~\      ~~---l__i__i__i--~~_/
+                                 _-~-__   ~)  \--______________--~~
+                               //.-~~~-~_--~- |-------~~~~~~~~
+                                      //.-~~~--\
+                               神兽保佑
+                              代码无BUG!
+
+"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
