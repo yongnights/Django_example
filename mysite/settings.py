81049f00b445c68c757ab9b01d04db6725397bcb
Xxx <xxx@xxx.com>
2020-05-18 12:13:43 +0800

    开启功能，新增部署方式文件

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admindocs',
     'polls.apps.PollsConfig',
     'login.apps.LoginConfig',
     'captcha',
